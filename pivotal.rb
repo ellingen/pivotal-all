28151dedcb2a8fe9e2a704a7c9bfaf0d1aad59b9
Benjamin Mateev <ben@urbanvention.com>
2011-06-21 01:26:00 +0200

    working version

--- pivotal.rb
+++ pivotal.rb
@@ -3,13 +3,15 @@
 # require 'sinatra'
 # require 'pivotal-tracker'
 
+require "sinatra/config_file"
+
 # set utf-8 for outgoing
 before do
   headers "Content-Type" => "text/html; charset=utf-8"
 end
 
 configure do
-  config_file "settings.yml"
+  
 end
 
 get "/css/:stylesheet.css" do
@@ -19,9 +21,10 @@ end
 
 
 get '/' do
-  PivotalTracker::Client.token = 'd66e0691ee1d86da00c1ced58587cbc6'
+  PivotalTracker::Client.token = "d66e0691ee1d86da00c1ced58587cbc6"
   PivotalTracker::Client.use_ssl = true
   @projects = PivotalTracker::Project.all
+
   haml :index
 end
 
