cc25ade8434e5236308ea1415fefc2d462060402
Benjamin Mateev <ben@urbanvention.com>
2011-06-21 00:18:55 +0200

    some changes

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# Source
+source :rubygems
+
+# the framework
+gem "sinatra"
+
+# the style
+gem "haml"
+gem "sass"
+
+# the api
+gem "pivotal-tracker"
--- pivotal.rb
+++ pivotal.rb
@@ -3,7 +3,7 @@ require 'pivotal-tracker'
 require 'erb'
 
 get '/' do
-  PivotalTracker::Client.token = 'd66e0691ee1d86da00c1ced58587cbc6'
+  PivotalTracker::Client.token = ''
   PivotalTracker::Client.use_ssl = true
   @projects = PivotalTracker::Project.all
   
